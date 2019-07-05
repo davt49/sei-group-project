7a354e96dab13b52bc6faa36fa67bc7a3f8b91fa
MiaLearnsToCode <meroimia@gmail.com>
2019-07-05 10:34:34 +0100

    commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const express = require('express')
+const { port, dbURI } = require('./config/environment')
+const app = express()
 
+const mongoose = require('mongoose')
+mongoose.connect(dbURI, { useNewUrlParser: true }, () => console.log(dbURI))
+
+app.listen(port, () => console.log(port))
