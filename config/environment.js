09e83bfa731a695024168b6079a3f854205fbb19
davidtsui <davidtsuiart@gmail.com>
2019-07-08 11:52:12 +0100

    weekend

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/han-solo'
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/han-solo${process.env.NODE_ENV || 'dev'}`
 const secret = process.env.SECRET || 'hansolo'
 
 module.exports = { port, dbURI, secret }
