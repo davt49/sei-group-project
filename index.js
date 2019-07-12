4189dac2d59f9abaf110d856686aabb04ca0a4e8
davidtsui <davidtsuiart@gmail.com>
2019-07-12 11:04:33 +0100

    first master commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const errorHandler = require('./lib/errorHandler')
 
 mongoose.connect(dbURI, { useNewUrlParser: true }, () => console.log(dbURI))
 
+app.use(express.static(`${__dirname}/dist`))
+
 app.use(bodyParser.json())
 
 app.use(logger)
@@ -16,6 +18,8 @@ app.use(logger)
 app.use('/api', router)
 
 app.use(errorHandler)
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 app.listen(port, () => console.log(port))
 
